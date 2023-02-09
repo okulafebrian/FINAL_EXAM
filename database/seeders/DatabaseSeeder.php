6c6423e4fa6fa19b29eb1b5f4bffd9736dddf6b5
okulafebrian <okulafebrian@gmail.com>
2023-02-09 15:46:08 +0700

    remove UserSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
             GenderSeeder::class,
             RoleSeeder::class,
             ItemSeeder::class,
-            UserSeeder::class,
         ]);
     }
 }
